c37db4de972e05a2a49a542c96a321d56ac6fd85
Paruhas <bayulgang@hotmail.com>
2021-08-28 00:43:26 +0700

    improve foodList() and add 1 data in json

--- index.js
+++ index.js
@@ -12,39 +12,63 @@ var __assign = (this && this.__assign) || function () {
 var Guru_Cooking_Box = require("./guruCookingList.json");
 var calEXP = function (name) {
     var arrCalEXP = Guru_Cooking_Box.reduce(function (acc, cur) {
-        var ingredientEXP = { totalIngredientEXP_D1: 0 };
-        var ingredientEXPD2 = 0;
+        var totalIngredientEXP_D1 = 0;
+        var totalIngredientEXP_D2 = 0;
         if (cur.name === name) {
+            // // this level is calculate "MAIN FOOD" exp
             var materialEXP = cur.recipe.reduce(function (acc1, cur1) {
-                if (cur1.exp === 0) {
+                // // this level is calculate "MAIN MATERIAL" of MAIN FOOD exp
+                // console.log(cur1);
+                if (typeof cur1 === "object" && cur1.recipe.length !== 0) {
+                    var totalIngredientEXP_D1_reduce = cur1.recipe.reduce(function (accD1, curD1) {
+                        // // this level is calculate "INGREDIENT MATERIAL" of MAIN MATERIAL exp - D1
+                        // console.log(curD1);
+                        if (typeof curD1 === "object" && curD1.recipe.length !== 0) {
+                            var totalIngredientEXP_D2_reduce = curD1.recipe.reduce(function (accD2, curD2) {
+                                // // this level is calculate "INGREDIENT MATERIAL" of INGREDIENT MATERIAL exp - D2
+                                // console.log(curD2);
+                                if (curD2.exp * curD2.quantity === 0 ||
+                                    !curD2.exp ||
+                                    !curD2.quantity) {
+                                    acc.missingSomeExp = true;
+                                }
+                                accD2.sumIngredientEXP_D2 =
+                                    accD2.sumIngredientEXP_D2 +
+                                        curD2.exp * curD2.quantity;
+                                return accD2;
+                            }, { sumIngredientEXP_D2: 0 });
+                            totalIngredientEXP_D2 =
+                                totalIngredientEXP_D2 +
+                                    totalIngredientEXP_D2_reduce.sumIngredientEXP_D2;
+                        }
+                        if (curD1.exp * curD1.quantity === 0 ||
+                            !curD1.exp ||
+                            !curD1.quantity) {
+                            acc.missingSomeExp = true;
+                        }
+                        accD1.sumIngredientEXP_D1 =
+                            accD1.sumIngredientEXP_D1 +
+                                (curD1.exp ? curD1.exp : 0) *
+                                    (curD1.quantity ? curD1.quantity : 0);
+                        return accD1;
+                    }, { sumIngredientEXP_D1: 0 });
+                    totalIngredientEXP_D1 =
+                        totalIngredientEXP_D1 +
+                            totalIngredientEXP_D1_reduce.sumIngredientEXP_D1;
+                }
+                if (cur1.exp * cur1.quantity === 0 || !cur1.exp || !cur1.quantity) {
                     acc.missingSomeExp = true;
                 }
-                if (cur1.recipe.length !== 0) {
-                    var ingredientEXP_D1_reduce = cur1.recipe.reduce(function (accD1, curD1) {
-                        if (curD1.recipe.length !== 0) {
-                            var ingredientEXP_D2_reduce = curD1.recipe.reduce(function (accD2, curD2) {
-                                accD2.totalIngredientEXP_D2 =
-                                    curD2.exp * curD2.quantity;
-                                return accD2;
-                            }, { totalIngredientEXP_D2: 0 });
-                            ingredientEXPD2 =
-                                ingredientEXP_D2_reduce.totalIngredientEXP_D2;
-                        }
-                        accD1.totalIngredientEXP_D1 =
-                            accD1.totalIngredientEXP_D1 + curD1.exp * curD1.quantity;
-                        return accD1;
-                    }, { totalIngredientEXP_D1: 0 });
-                    ingredientEXP = ingredientEXP_D1_reduce;
-                }
                 acc1.totalMaterialEXP =
-                    acc1.totalMaterialEXP + cur1.exp * cur1.quantity;
+                    acc1.totalMaterialEXP +
+                        (cur1.exp ? cur1.exp : 0) * (cur1.quantity ? cur1.quantity : 0);
                 return acc1;
             }, { totalMaterialEXP: 0 });
             acc.totalEXP =
                 cur.exp +
                     materialEXP.totalMaterialEXP +
-                    ingredientEXP.totalIngredientEXP_D1 +
-                    ingredientEXPD2;
+                    totalIngredientEXP_D1 +
+                    totalIngredientEXP_D2;
         }
         return acc;
     }, { totalEXP: 0, missingSomeExp: false });
--- index.ts
+++ index.ts
@@ -3,48 +3,81 @@ var Guru_Cooking_Box = require("./guruCookingList.json");
 
 const calEXP = (name: string) => {
   const arrCalEXP = Guru_Cooking_Box.reduce(
-    (acc, cur) => {
-      let ingredientEXP = { totalIngredientEXP_D1: 0 };
-      let ingredientEXPD2 = 0;
+    (acc: any, cur: any) => {
+      let totalIngredientEXP_D1: number = 0;
+      let totalIngredientEXP_D2: number = 0;
 
       if (cur.name === name) {
-        const materialEXP = cur.recipe.reduce(
-          (acc1, cur1) => {
-            if (cur1.exp === 0) {
-              acc.missingSomeExp = true;
-            }
+        // // this level is calculate "MAIN FOOD" exp
 
-            if (cur1.recipe.length !== 0) {
-              const ingredientEXP_D1_reduce = cur1.recipe.reduce(
-                (accD1, curD1) => {
-                  if (curD1.recipe.length !== 0) {
-                    const ingredientEXP_D2_reduce = curD1.recipe.reduce(
-                      (accD2, curD2) => {
-                        accD2.totalIngredientEXP_D2 =
+        const materialEXP = cur.recipe.reduce(
+          (acc1: any, cur1: any) => {
+            // // this level is calculate "MAIN MATERIAL" of MAIN FOOD exp
+            // console.log(cur1);
+
+            if (typeof cur1 === "object" && cur1.recipe.length !== 0) {
+              const totalIngredientEXP_D1_reduce = cur1.recipe.reduce(
+                (accD1: any, curD1: any) => {
+                  // // this level is calculate "INGREDIENT MATERIAL" of MAIN MATERIAL exp - D1
+                  // console.log(curD1);
+
+                  if (typeof curD1 === "object" && curD1.recipe.length !== 0) {
+                    const totalIngredientEXP_D2_reduce = curD1.recipe.reduce(
+                      (accD2: any, curD2: any) => {
+                        // // this level is calculate "INGREDIENT MATERIAL" of INGREDIENT MATERIAL exp - D2
+                        // console.log(curD2);
+
+                        if (
+                          curD2.exp * curD2.quantity === 0 ||
+                          !curD2.exp ||
+                          !curD2.quantity
+                        ) {
+                          acc.missingSomeExp = true;
+                        }
+
+                        accD2.sumIngredientEXP_D2 =
+                          accD2.sumIngredientEXP_D2 +
                           curD2.exp * curD2.quantity;
 
                         return accD2;
                       },
-                      { totalIngredientEXP_D2: 0 }
+                      { sumIngredientEXP_D2: 0 }
                     );
 
-                    ingredientEXPD2 =
-                      ingredientEXP_D2_reduce.totalIngredientEXP_D2;
+                    totalIngredientEXP_D2 =
+                      totalIngredientEXP_D2 +
+                      totalIngredientEXP_D2_reduce.sumIngredientEXP_D2;
                   }
 
-                  accD1.totalIngredientEXP_D1 =
-                    accD1.totalIngredientEXP_D1 + curD1.exp * curD1.quantity;
+                  if (
+                    curD1.exp * curD1.quantity === 0 ||
+                    !curD1.exp ||
+                    !curD1.quantity
+                  ) {
+                    acc.missingSomeExp = true;
+                  }
+
+                  accD1.sumIngredientEXP_D1 =
+                    accD1.sumIngredientEXP_D1 +
+                    (curD1.exp ? curD1.exp : 0) *
+                      (curD1.quantity ? curD1.quantity : 0);
 
                   return accD1;
                 },
-                { totalIngredientEXP_D1: 0 }
+                { sumIngredientEXP_D1: 0 }
               );
 
-              ingredientEXP = ingredientEXP_D1_reduce;
+              totalIngredientEXP_D1 =
+                totalIngredientEXP_D1 +
+                totalIngredientEXP_D1_reduce.sumIngredientEXP_D1;
+            }
+            if (cur1.exp * cur1.quantity === 0 || !cur1.exp || !cur1.quantity) {
+              acc.missingSomeExp = true;
             }
 
             acc1.totalMaterialEXP =
-              acc1.totalMaterialEXP + cur1.exp * cur1.quantity;
+              acc1.totalMaterialEXP +
+              (cur1.exp ? cur1.exp : 0) * (cur1.quantity ? cur1.quantity : 0);
 
             return acc1;
           },
@@ -54,8 +87,8 @@ const calEXP = (name: string) => {
         acc.totalEXP =
           cur.exp +
           materialEXP.totalMaterialEXP +
-          ingredientEXP.totalIngredientEXP_D1 +
-          ingredientEXPD2;
+          totalIngredientEXP_D1 +
+          totalIngredientEXP_D2;
       }
 
       return acc;
@@ -67,7 +100,7 @@ const calEXP = (name: string) => {
 };
 
 const foodList = (name: string = "") => {
-  const arrFoodName = Guru_Cooking_Box.reduce((acc, cur) => {
+  const arrFoodName = Guru_Cooking_Box.reduce((acc: any, cur: any) => {
     if (name === "") {
       const sumEXP = calEXP(cur.name);
 
@@ -97,7 +130,7 @@ const foodList = (name: string = "") => {
     return acc;
   }, []);
 
-  arrFoodName.sort((a, b) => b.totalExp - a.totalExp);
+  arrFoodName.sort((a: any, b: any) => b.totalExp - a.totalExp);
 
   return arrFoodName;
 };
